017b1c57403480bdffcea7f9e4aee650df152a72
abelelol <abeleamsalu@gmail.com>
2020-01-22 20:42:35 -0500

    added audio recording functionality

--- dr_sounds/app/src/main/java/com/example/dr_sounds/AddFamilyMem.java
+++ dr_sounds/app/src/main/java/com/example/dr_sounds/AddFamilyMem.java
@@ -1,5 +1,6 @@
 package com.example.dr_sounds;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -15,19 +16,24 @@ public class AddFamilyMem extends AppCompatActivity {
 //    SpeakerVerificationClient stuff;
 //    SpeakerVerificationRestClient enrolment;
 //    SpeakerIdentificationClient hey;
+    Button addsound;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.add_family);
-//        enrolment = new SpeakerVerificationRestClient("7f42c853e26744588483b36f10a4abc4");
-
+        addsound = findViewById(R.id.add_sound);
+        addsound.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(AddFamilyMem.this, AudioRecord.class);
+                startActivity(intent);
+            }
+        });
     }
 
     // this is going to method connected to the onclick of the add sound button
-    public void addSound(View v){
-
-
-
-
-    }
+//    public void addSound(View v){
+//        Intent intent = new Intent(AddFamilyMem.this, AudioRecord.class);
+//        startActivity(intent);
+//    }
 }
--- dr_sounds/app/src/main/java/com/example/dr_sounds/AudioRecord.java
+++ dr_sounds/app/src/main/java/com/example/dr_sounds/AudioRecord.java
@@ -1,20 +1,20 @@
 package com.example.dr_sounds;
+
 import android.Manifest;
 import android.content.Context;
 import android.content.pm.PackageManager;
 import android.media.MediaPlayer;
 import android.media.MediaRecorder;
 import android.os.Bundle;
-import
-
-
 import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.LinearLayout;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
 import java.io.IOException;
 
